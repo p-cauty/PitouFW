7759292893b147c5fb3463d182108672db26c6cd
PHPeter <phpeter@phpeter.fr>
2020-12-01 17:27:37 +0100

    Add self-hosted font-awesome

--- app/views/mainView.php
+++ app/views/mainView.php
@@ -9,6 +9,7 @@ use PitouFW\Core\Alert;
 		<title><?= NAME . (isset($TITLE) ? ' - ' . $TITLE : '') ?></title>
 		<meta charset="utf-8" />
 		<link type="text/css" rel="stylesheet" href="<?= CSS . 'bootstrap.min.css' ?>" media="screen" />
+        <link type="text/css" rel="stylesheet" href="<?= CSS . 'font-awesome_v5.15.1.min.css' ?>" media="screen" />
         <link type="text/css" rel="stylesheet" href="<?= CSS . 'style.css' ?>" media="screen" />
 		<meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1.0" />
 		<meta name="format-detection" content="telephone=no" />
@@ -23,7 +24,6 @@ use PitouFW\Core\Alert;
 
 		<script type="text/javascript" src="<?= JS . 'jquery.min.js' ?>"></script>
 		<script type="text/javascript" src="<?= JS . 'bootstrap.min.js' ?>"></script>
-        <script src="https://kit.fontawesome.com/95c47c14b9.js" crossorigin="anonymous"></script>
 		<script type="text/javascript" src="<?= JS . 'script.js' ?>"></script>
 	</body>
 </html>
