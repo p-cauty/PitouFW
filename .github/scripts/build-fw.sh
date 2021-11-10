5e269c847dc6c2eb28c5414295a9b9e592d2cb9f
phpitou <phpeter@phpeter.fr>
2021-11-10 20:02:47 +0100

    Remove build-fw action from fw artifact

--- .github/scripts/build-fw.sh
+++ .github/scripts/build-fw.sh
@@ -1,3 +1,3 @@
-rm -rf pitoufw/.git
+rm -rf pitoufw/.git pitoufw/.github/scripts/build-fw.sh pitoufw/.github/workflows/build-fw.yml
 touch release.tar.gz
 tar --exclude=release.tar.gz -zcvf release.tar.gz pitoufw
